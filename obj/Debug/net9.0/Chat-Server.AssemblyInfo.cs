be62734423f59e3898dc15b8ee9debf36dc771f9
Ryan Mendoza <ryan@Ryans-MacBook-Pro.local>
2025-12-23 03:24:14 -0500

    broadcasting fixes

--- obj/Debug/net9.0/Chat-Server.AssemblyInfo.cs
+++ obj/Debug/net9.0/Chat-Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chat-Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2d605b5bf76ea7112d38ac064711e3847910860")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f4bdb1500c586e329b1e19a21afcb0035a389b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chat-Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chat-Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
